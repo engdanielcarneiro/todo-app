8324a7cccabf4fd716ac2fe489050c4012fd0e79
Carneiro <daniel.carneiro@accruent.com>
2024-08-17 23:14:33 -0300

    feat: creating page layout

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import styles from './App.module.css'
+
+import './global.css'
+
+import todoLogo from '../src/assets/todo-logo.svg'
+import { PlusCircle } from '@phosphor-icons/react'
+
+function App() {
+
+  return (
+    <>
+      <header className={styles.header}>
+        <img src={todoLogo} alt='Logotipo do aplicativo todo' />
+      </header>
+      <div>
+        <main>
+          <form>
+            <div className={styles.addTaskContainer}>
+              <input type="text" placeholder='Adicione uma nova tarefa' />
+              <button>
+                Criar
+                <PlusCircle size={'1rem'}/>
+              </button>
+            </div>
+          </form>
+          <div className={styles.mainContentContainer}>
+            <div className={styles.tarefasStatusContainer}>
+              <div className={styles.statusItem} style={{color: 'var(--blue)'}}>
+                Tarefas criadas
+                <div className={styles.countContainer}>0</div>
+              </div>
+              <div className={styles.statusItem} style={{color: 'var(--purple)'}}>
+                Concluídas
+                <div className={styles.countContainer}>0</div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
